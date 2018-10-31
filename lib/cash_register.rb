bfc9f617abe07887f7006e44d378e6e21ac329d8
Paul Fell-Cinoman <swellpf@gmail.com>
2018-10-31 21:16:28 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class CashRegister
   attr_accessor :total, :discount, :items, :last_transaction
   
@@ -16,8 +17,9 @@ class CashRegister
   end
   
   def apply_discount
+    binding.pry
     if discount != 0
-      self.total = (total * (100 - discount.to_f).to_i)
+      self.total = (total * (100 - discount.to_f)/100).to_i)
        "After the discount, the total comes to $#{self.total}."
     else
        "There is no discount to apply."
